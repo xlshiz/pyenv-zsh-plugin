5fe36805130e1e8f0d13f3448fc668e5bdd32ff2
shixl <xlshiz@users.noreply.github.com>
2019-04-16 22:32:22 +0800

    Don't set PATH

--- pyenv.plugin.zsh
+++ pyenv.plugin.zsh
@@ -1,7 +1,7 @@
 # This plugin loads pyenv into the current shell and provides prompt info via
 # the 'pyenv_prompt_info' function. Also loads pyenv-virtualenv if available.
 
-FOUND_PYENV=0
+FOUND_PYENV=1
 
 if [[ $FOUND_PYENV -ne 1 ]]; then
     pyenvdirs=("$HOME/.pyenv" "/usr/local/pyenv" "/opt/pyenv")
